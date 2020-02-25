79304e9f878efc8c02ff99346f1cbda58f324ca3
timwoo112 <timwoo112@gmail.com>
2020-02-25 13:23:48 -0500

    Added linear and binary search programs

---
+++ practice/calculator_practice/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Declare functions
+int showMenu(int);
+float add(float, float);
+float subtract(float, float);
+float multiply(float, float);
+float divide(float, float);
+
+int main()
+{
+
+  return 0
+}
+
+int showMenu(int selection)
+{
+ 
+}
+
+float add(float num1, float num2)
+{
+  return num1 + num2;
+}
+
+float subtract(float num1, float num2)
+{
+  return num1 - num2;
+}
+
+float multiply(float num1, float num2)
+{
+  return num1 * num2;
+}
+
+float divide(float num1, float num2)
+{
+  return num1 / num2;
+}
